73f3c1b89380f435787226e5ac3f49aab4d6cc59
jntm <262448350@qq.com>
2024-06-08 02:59:39 +0800

    init spring success

---
+++ readme.txt
@@ -0,0 +1,6 @@
+1 創建 quick start project
+2 jdk 17
+3 spring framework 5.2.23.RELEASE
+
+
+4 快捷键:  idea 快捷键上需要 Fn 建
---
+++ src/test/java/com/msb/AppTest.java
@@ -0,0 +1,48 @@
+package com.msb;
+
+import com.msb.bean.Person;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp(){
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("ioc.xml");
+        Person person1 = context.getBean("person", Person.class);
+        System.out.println("person1 = " + person1);
+
+        Person person2 = context.getBean("person", Person.class);
+        System.out.println("person2 = " + person2);
+        // scope="singleton"  person1 == person2 ？true
+        System.out.println("person1 == person2 ？" + (person1 == person2));
+    }
+}
